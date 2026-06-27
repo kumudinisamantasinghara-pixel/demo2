12597feaf145d73786743d53479124c9b97bbaad
kumudinisamantasinghara-pixel <kumudinisamantasinghara@gmail.com>
2026-06-27 08:56:50 +0530

    add changes

---
+++ Greet.java
@@ -0,0 +1,6 @@
+package demo2;
+public class Greet {
+    public static void main(String[] args) {
+        System.err.println("java technocrat");
+    }
+}
---
+++ git.java
@@ -0,0 +1,6 @@
+package demo2;
+public class git{
+   public static void main(String[] args) {
+    System.out.println ("java technocrat");
+   } 
+}
